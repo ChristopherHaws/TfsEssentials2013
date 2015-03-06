269d8bf125d8e08d1b0ca41502d810e0621cc836
ChristopherHaws <cribs2@gmail.com>
2015-03-06 00:03:51 -0800

    Added outgoing changesets section

---
+++ TfsEssentials2013/Controls/Merge/Outgoing/OutgoingChangesetsSectionView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Spiral.TfsEssentials.Controls.Merge.Outgoing
+{
+	/// <summary>
+	/// Interaction logic for OutgoingChangesetsSectionView.xaml
+	/// </summary>
+	internal partial class OutgoingChangesetsSectionView : UserControl
+	{
+		public OutgoingChangesetsSectionView()
+		{
+			InitializeComponent();
+		}
+	}
+}
